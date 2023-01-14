e04f4f7b875ef1274d77089cb88121d82968ede3
AndrewYatzkan <andrew@yatzkan.com>
2023-01-13 21:42:38 -0600

    Relative paths

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,3 @@
-/*
-// https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
-Geolocation code:
-
-const options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
-};
-
-function success(pos) {
-  const crd = pos.coords;
-
-  console.log('Your current position is:');
-  console.log(`Latitude : ${crd.latitude}`);
-  console.log(`Longitude: ${crd.longitude}`);
-  console.log(`More or less ${crd.accuracy} meters.`);
-}
-
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
-
-navigator.geolocation.getCurrentPosition(success, error, options);
-*/
-
 document.body.onclick = () => DeviceMotionEvent.requestPermission();
 
 async function init() {
