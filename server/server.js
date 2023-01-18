df368e08bc12e045fdf6a3712c174fc0341dd567
AndrewYatzkan <andrew@yatzkan.com>
2023-01-17 22:07:37 -0600

    Set to current time

--- server/server.js
+++ server/server.js
@@ -7,16 +7,16 @@ const port = 3000;
 const { scrapeTweets } = require('./twitter');
 const Tweet = require('./models/Tweet');
 
-// scrapeTweets();
-// setInterval(scrapeTweets, 20 * 60 * 1_000); // 20 mins
+scrapeTweets();
+setInterval(scrapeTweets, 20 * 60 * 1_000); // 20 mins
 
 
 app.use(express.static(`${__dirname}/../public`));
 
 // gets events that haven't ended
 app.get('/events', async (req, res) => {
-  // let time = Date.now();
-  let time = 1670860900000;
+  let time = Date.now();
+  // let time = 1670860900000;
   let events = await Tweet.find({end_timestamp: {$gt: time}});
   res.send(events);
 });
